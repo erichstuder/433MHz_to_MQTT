70ea1cc3469acb9303c5f52c74d2850e0dbdbf71
erichstuder <richu_@hotmail.com>
2024-04-20 17:50:36 +0200

    unit test

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,36 +1,24 @@
 /*
-* Everything in this library shall be runnable on any target, at least on:
+* Everything in this library shall be runnable on any target, but at least on:
 * - thumbv6m-none-eabi
 * - x86_64-unknown-linux-gnu
 */
 
 #![cfg_attr(not(test), no_std)]
 
-#[cfg(test)]
-use mockall::*;
+extern crate alloc;
+use alloc::boxed::Box;
 
-#[cfg(test)]
-use mockall::predicate::*;
-
-#[cfg_attr(test, automock)]
-pub trait ParserTrait {
-    fn send_message(&self, msg: &[u8]);
-    fn enter_bootlaoder(&self);
+pub struct Parser {
+    send_message: Box<dyn FnMut(&[u8])>,
+    enter_bootloader: Box<dyn FnMut()>,
 }
 
-pub struct Parser<'a> {
-     parser_trait: &'a dyn ParserTrait,
-}
-
-impl Parser<'_> {
-    pub fn new(parser_trait: &'static impl ParserTrait) {
-        Self {parser_trait};
-    }
-
-    pub fn parse_message(&self, msg: &[u8]) {
+impl Parser {
+    pub fn parse_message(&mut self, msg: &[u8]) {
         if msg.eq(b"enter bootloader") {
-            self.parser_trait.send_message(b"entering bootloader now\n");
-            self.parser_trait.enter_bootlaoder();
+            (self.send_message)(b"entering bootloader now\n");
+            (self.enter_bootloader)();
         }
     }
 }
@@ -38,18 +26,23 @@ impl Parser<'_> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::sync::atomic::{Ordering, AtomicBool};
 
     #[test]
-    fn enter_bootloader_test() {
-        let mut mock = MockParserTrait::new();
+    // TODO: This is not a great test. But as I'm still learning, I leave it like this for now.
+    fn test_enter_bootloader() {
+        fn mock_send_message(_msg: &[u8]) {}
+        //static mut RESULT_MSG: &[u8];
+        static ENTER_BOOTLOADER_CALLED: AtomicBool = AtomicBool::new(false);
 
-        let text = b"Hello";
+        let mut parser = Parser {
+            send_message: Box::new(mock_send_message),
+            //send_message: Box::new(|msg: &[u8]| RESULT_MSG = msg),
+            enter_bootloader: Box::new(|| ENTER_BOOTLOADER_CALLED.store(true, Ordering::Relaxed)),
+        };
 
-        mock.expect_send_message()
-            .times(1)
-            .withf(|msg: &[u8]| msg.eq(text))
-            .return_const(());
+        parser.parse_message(b"enter bootloader");
 
-        mock.send_message(text);
+        assert!(ENTER_BOOTLOADER_CALLED.load(Ordering::Relaxed));
     }
 }

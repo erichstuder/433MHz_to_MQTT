795b038a485f7cb2dd7b6cd1e898a204b90b1916
erichstuder <richu_@hotmail.com>
2024-05-09 21:26:33 +0200

    remove setting to prevent warning

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,6 +23,5 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
 
 drawio_no_sandbox = True

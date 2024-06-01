5d3b51fa9fde3c52eb84566acfd5002d9c51fa33
erichstuder <richu_@hotmail.com>
2024-06-01 15:17:50 +0200

    also move all the communication stuff outside

--- software/firmware/src/main.rs
+++ software/firmware/src/main.rs
@@ -1,70 +1,14 @@
 #![no_std]
 #![no_main]
 
-mod usb_configuration;
+mod usb_communication;
 
 use {defmt_rtt as _, panic_probe as _};
 use embassy_executor::Spawner;
-use embassy_futures::join::join;
 //use embassy_rp::usb::{Driver, Instance, InterruptHandler};
-use embassy_usb::class::cdc_acm::State;
-use embassy_usb::driver::EndpointError;
-use app;
-use usb_configuration::UsbConfiguration;
-
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_rp::init(Default::default());
-
-    let mut state = State::new();
-    let mut usb_device = UsbConfiguration::new(p.USB, &mut state);
-
-    let echo_fut = async {
-        loop {
-            usb_device.cdc_acm_class.wait_connection().await;
-            let _ = echo(&mut usb_device.cdc_acm_class).await;
-        }
-    };
-
-    // Run everything concurrently.
-    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
-    join(usb_device.usb.run(), echo_fut).await;
-}
-
-struct Disconnected {}
-
-impl From<EndpointError> for Disconnected {
-    fn from(val: EndpointError) -> Self {
-        match val {
-            EndpointError::BufferOverflow => panic!("Buffer overflow"),
-            EndpointError::Disabled => Disconnected {},
-        }
-    }
-}
-
-async fn echo<'d, T: embassy_rp::usb::Instance + 'd>(class: &mut embassy_usb::class::cdc_acm::CdcAcmClass<'d, embassy_rp::usb::Driver<'d, T>>) -> Result<(), Disconnected> {
-    let mut buf = [0; 64];
-
-    struct EnterBootloaderImpl;
-
-    impl app::EnterBootloader for EnterBootloaderImpl {
-        fn call(&mut self) {
-            embassy_rp::rom_data::reset_to_usb_boot(0, 0);
-        }
-    }
-
-    let mut parser = app::Parser::new(EnterBootloaderImpl);
-
-    loop {
-        let n = class.read_packet(&mut buf).await?;
-        let data = &buf[..n];
-        class.write_packet(b"echo: ").await?;
-        class.write_packet(data).await?;
-        class.write_packet(b"\n").await?;
-
-        let answer = parser.parse_message(data);
-        class.write_packet(answer).await?;
-        class.write_packet(b"\n\n").await?;
-    }
+    usb_communication::run().await;
+    //hier können andere futures verwendet werden z.B. mit futures::join!(usb_communication::run(), other_future);
 }
--- software/firmware/src/usb_communication.rs
+++ software/firmware/src/usb_communication.rs
@@ -2,6 +2,9 @@ use embassy_rp::peripherals::USB;
 use embassy_rp::usb::Driver;
 use embassy_usb::{Builder, Config, UsbDevice};
 use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use embassy_usb::driver::EndpointError;
+use app;
+use embassy_futures::join::join;
 
 embassy_rp::bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => embassy_rp::usb::InterruptHandler<embassy_rp::peripherals::USB>;
@@ -55,4 +58,56 @@ impl<'a> UsbConfiguration<'a> {
             usb,
         }
     }
+}
+
+struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+async fn echo<'d, T: embassy_rp::usb::Instance + 'd>(class: &mut embassy_usb::class::cdc_acm::CdcAcmClass<'d, embassy_rp::usb::Driver<'d, T>>) -> Result<(), Disconnected> {
+    let mut buf = [0; 64];
+
+    struct EnterBootloaderImpl;
+
+    impl app::EnterBootloader for EnterBootloaderImpl {
+        fn call(&mut self) {
+            embassy_rp::rom_data::reset_to_usb_boot(0, 0);
+        }
+    }
+
+    let mut parser = app::Parser::new(EnterBootloaderImpl);
+
+    loop {
+        let n = class.read_packet(&mut buf).await?;
+        let data = &buf[..n];
+        class.write_packet(b"echo: ").await?;
+        class.write_packet(data).await?;
+        class.write_packet(b"\n").await?;
+
+        let answer = parser.parse_message(data);
+        class.write_packet(answer).await?;
+        class.write_packet(b"\n\n").await?;
+    }
+}
+
+pub async fn run() {
+    let p = embassy_rp::init(Default::default());
+    let mut state = State::new();
+    let mut usb_device = UsbConfiguration::new(p.USB, &mut state);
+    let echo_fut = async {
+        loop {
+            usb_device.cdc_acm_class.wait_connection().await;
+            let _ = echo(&mut usb_device.cdc_acm_class).await;
+        }
+    };
+    // Run everything concurrently.
+    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
+    join(usb_device.usb.run(), echo_fut).await;
 }

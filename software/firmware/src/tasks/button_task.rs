ea35d57e817ae137284f2bb1fa5e3d376500da48
erichstuder <richu_@hotmail.com>
2025-03-09 16:58:09 +0100

    refactoring to reduce size of main.rs

---
+++ software/firmware/src/tasks/button_task.rs
@@ -0,0 +1,31 @@
+use embassy_executor::task;
+use embassy_rp::pio::Pio;
+use embassy_rp::peripherals::{PIO0, PIN_28};
+
+use crate::drivers::remote_receiver::RemoteReceiver;
+use crate::UsbSenderMutex;
+
+use app::buttons::Buttons;
+
+#[task]
+pub async fn run(mut pio: Pio<'static, PIO0>, receiver_pin: PIN_28, usb_sender: &'static UsbSenderMutex) {
+    // It would be nice to have generic types for pio and receiver_pin but I couldn't figure out how to do it.
+
+    let buttons = Buttons::new();
+
+    let mut remote_receiver = RemoteReceiver::new(
+        &mut pio.common,
+        pio.sm0,
+        receiver_pin,
+        buttons,
+    );
+
+    loop {
+        let pressed_button = remote_receiver.read().await;
+        {
+            let mut sender = usb_sender.lock().await;
+            let _ = sender.write_packet(pressed_button.as_bytes()).await;
+            let _ = sender.write_packet(b"\n").await;
+        }
+    }
+}
---
+++ software/firmware/src/tasks/mod.rs
@@ -0,0 +1,2 @@
+pub mod button_task;
+pub mod echo_task;

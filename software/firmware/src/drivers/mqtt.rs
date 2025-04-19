9dc338af244f48ff7c6a09698b201ce6d1f9431d
erichstuder <richu_@hotmail.com>
2025-04-19 16:19:11 +0200

    unit tests and cleanup

--- software/firmware/src/drivers/mqtt.rs
+++ software/firmware/src/drivers/mqtt.rs
@@ -2,56 +2,56 @@ use cfg_if::cfg_if;
 
 cfg_if! {
     if #[cfg(not(test))] {
+        use defmt::{info, error};
         use embassy_executor::{task, Spawner};
         use embassy_rp::gpio;
         use embassy_time::{Duration, Timer};
         use embassy_net;
         use embassy_rp::clocks::RoscRng;
+        use embassy_rp::pio::Pio;
+        use embassy_rp::peripherals::{DMA_CH1, PIO1, PIN_23, PIN_24, PIN_25, PIN_29};
         use rand_core::RngCore; // Don't know why this is needed. Is it because the 'use' is missing in embassy_rp::clocks::RoscRng?
         use static_cell::StaticCell;
         use cyw43_pio::DEFAULT_CLOCK_DIVIDER;
         use cyw43::JoinOptions;
         use core::net::Ipv4Addr;
+        use heapless::String;
+        use rust_mqtt::client::client::MqttClient;
+        use rust_mqtt::utils::rng_generator::CountingRng;
+        use embassy_sync::mutex::Mutex;
+        use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+
+        use crate::PersistencyMutexed;
+        use crate::drivers::persistency;
+
+        type MqttClientMutexed = Mutex<CriticalSectionRawMutex, MqttClient<'static, embassy_net::tcp::TcpSocket<'static>, 5, CountingRng>>;
+
+        pub struct WifiHw {
+            pub pin_23: PIN_23,
+            pub pin_24: PIN_24,
+            pub pin_25: PIN_25,
+            pub pin_29: PIN_29,
+            pub pio_1: Pio<'static, PIO1>,
+            pub dma_ch1: DMA_CH1,
+        }
+
+        const MQTT_BROKER_USERNAME_LENGTH: usize = 32;
+        const MQTT_BROKER_PASSWORD_LENGTH: usize = 64;
+
+        struct Credentials {
+            wifi_ssid: String<32>,
+            wifi_password: String<32>,
+            mqtt_host_ip: String<32>,
+            mqtt_broker_username: String<MQTT_BROKER_USERNAME_LENGTH>,
+            mqtt_broker_password: String<MQTT_BROKER_PASSWORD_LENGTH>,
+        }
     }
 }
 
-use defmt::{info, error};
-
-use embassy_rp::pio::Pio;
-use embassy_rp::peripherals::{DMA_CH1, PIO1, PIN_23, PIN_24, PIN_25, PIN_29};
-use rust_mqtt::utils::rng_generator::CountingRng;
 use core::str;
-use rust_mqtt::client::client::MqttClient;
-
-use crate::drivers::persistency;
-use crate::PersistencyMutexed;
-
-use heapless::String;
-use embassy_sync::mutex::Mutex;
-use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
-
-pub struct WifiHw {
-    pub pin_23: PIN_23,
-    pub pin_24: PIN_24,
-    pub pin_25: PIN_25,
-    pub pin_29: PIN_29,
-    pub pio_1: Pio<'static, PIO1>,
-    pub dma_ch1: DMA_CH1,
-}
-
-type MqttClientMutexed = Mutex<CriticalSectionRawMutex, MqttClient<'static, embassy_net::tcp::TcpSocket<'static>, 5, CountingRng>>;
-
-const MQTT_BROKER_USERNAME_LENGTH: usize = 32;
-const MQTT_BROKER_PASSWORD_LENGTH: usize = 64;
-struct Credentials {
-    wifi_ssid: String<32>,
-    wifi_password: String<32>,
-    mqtt_host_ip: String<32>,
-    mqtt_broker_username: String<MQTT_BROKER_USERNAME_LENGTH>,
-    mqtt_broker_password: String<MQTT_BROKER_PASSWORD_LENGTH>,
-}
 
 pub struct MQTT {
+    #[cfg(not(test))]
     client_mutexed: &'static MqttClientMutexed,
 }
 
@@ -121,7 +121,7 @@ impl MQTT {
         }
         info!("DHCP is now up!");
 
-        let (ip0, ip1, ip2, ip3) = Self::parse_ip(&credentials).unwrap();
+        let (ip0, ip1, ip2, ip3) = Self::parse_ip(&credentials.mqtt_host_ip).unwrap();
         let address = Ipv4Addr::new(ip0, ip1, ip2, ip3);
         let remote_endpoint = (address, 1883);
 
@@ -186,6 +186,8 @@ impl MQTT {
         })
     }
 
+    // TODO: Test for this function as soon as PersistencyMutexed can easily be mocked, if ever.
+    #[cfg(not(test))]
     async fn get_credentials(persistency_mutexed: &PersistencyMutexed, credentials: &mut Credentials) -> Result<(), &'static str> {
         let mut persistency = persistency_mutexed.lock().await;
 
@@ -228,20 +230,29 @@ impl MQTT {
         Ok(())
     }
 
-    fn parse_ip(credentials: &Credentials) -> Option<(u8, u8, u8, u8)> {
+    fn parse_ip(mqtt_host_ip: &str) -> Option<(u8, u8, u8, u8)> {
         let mut ip = [0u8; 4];
-        for (n, part) in credentials.mqtt_host_ip.as_bytes().split(|&b| b == b'.').enumerate() {
+        let mut count = 0;
+        for (n, part) in mqtt_host_ip.as_bytes().split(|&b| b == b'.').enumerate() {
             if n >= ip.len() {
+                #[cfg(not(test))]
                 error!("invalid mqtt host ip format");
-                return None;
+                return None
             }
             let part = str::from_utf8(part).unwrap();
             let part = part.parse::<u8>().unwrap();
             ip[n] = part;
+            count += 1;
+        }
+        if count != 4 {
+            #[cfg(not(test))]
+            error!("invalid mqtt host ip format");
+            return None
         }
         Some((ip[0], ip[1], ip[2], ip[3]))
     }
 
+    #[cfg(not(test))]
     pub async fn send_message(&mut self, payload: &[u8]) {
         let mut client = self.client_mutexed.lock().await;
         let result = client.send_message("433", payload, rust_mqtt::packet::v5::publish_packet::QualityOfService::QoS1, false).await;
@@ -275,6 +286,50 @@ async fn ping_task(client: &'static MqttClientMutexed) -> ! {
         match result {
             Ok(()) => info!("ping sent"),
             Err(mqtt_error) => info!("ping NOT sent: {:?}", mqtt_error),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_for_parse_ip {
+    use super::MQTT;
+    use std::panic;
+
+    #[test]
+    fn pass() {
+        let mqtt_host_ip = "123.55.6.2";
+        let (ip0, ip1, ip2, ip3) = MQTT::parse_ip(mqtt_host_ip).unwrap();
+        assert_eq!(ip0, 123);
+        assert_eq!(ip1, 55);
+        assert_eq!(ip2, 6);
+        assert_eq!(ip3, 2);
+    }
+
+    #[test]
+    fn too_long() {
+        let mqtt_host_ip = "123.55.6.2.42";
+        match MQTT::parse_ip(mqtt_host_ip) {
+            Some(_) => assert!(false, "Expected None, but got Some"),
+            None => assert!(true),
+        }
+    }
+
+    #[test]
+    fn too_short() {
+        let mqtt_host_ip = "123.55.6";
+        match MQTT::parse_ip(mqtt_host_ip) {
+            Some(_) => assert!(false, "Expected None, but got Some"),
+            None => assert!(true),
+        }
+    }
+
+    #[test]
+    fn letters() {
+        let mqtt_host_ip = "123.55.6.X";
+        let result = panic::catch_unwind(|| { MQTT::parse_ip(mqtt_host_ip) });
+        match result {
+            Ok(_) => assert!(false, "Expected panic, but got Ok"),
+            Err(_) => assert!(true),
         }
     }
 }

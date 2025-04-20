d44fbaaffc5b91368b3daab4604e49a2bdb1e3c9
erichstuder <richu_@hotmail.com>
2025-04-20 15:01:17 +0200

    add dependencies documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -116,5 +116,12 @@ def run_gherkindoc(app: Sphinx):
                     else:
                         f.write(line)
 
-def setup(app):
+def run_cargo_modules(app: Sphinx):
+    software_dependencies_path = os.path.join(app.srcdir, 'auto_generated/software_dependencies.png')
+    cargo_process = subprocess.Popen(['cargo', 'modules', 'dependencies', '--manifest-path', '../software/firmware',
+                      '--no-externs', '--no-fns', '--no-owns', '--no-types'], stdout=subprocess.PIPE)
+    subprocess.run(['dot', '-Tpng', '-o', software_dependencies_path], stdin=cargo_process.stdout, check=True)
+
+def setup(app: Sphinx):
     app.connect("builder-inited", run_gherkindoc)
+    app.connect("builder-inited", run_cargo_modules)

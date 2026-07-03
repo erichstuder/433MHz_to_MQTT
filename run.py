e3e18b14fbbd89d5affd892cc79f666d191d8f2e
erichstuder <richu_@hotmail.com>
2026-07-03 19:38:16 +0000

    upload via debugger

--- run.py
+++ run.py
@@ -83,25 +83,27 @@ if __name__ == '__main__':
             commands += ' -- --color always'
             commands += ' | tee target/unit-test-report.txt'
         elif arguments.upload:
-            # TODO: Maybe we could send the device into bootloader mode directly from inside the container?
-            import pyudev # Import only here, as this file is also used on github runners without hardware access. So this is not installed and won't be used there.
-            import serial
-            udev = pyudev.Context()
-            for usb_device in  udev.list_devices(subsystem="usb"):
-                if usb_device.attributes.get('manufacturer') == b'github.com/erichstuder' and usb_device.attributes.get('product') == b'433MHz_to_MQTT':
-                    for tty_device in  udev.list_devices(subsystem="tty"):
-                        if tty_device.sys_path.startswith(usb_device.sys_path):
-                            my_serial = serial.Serial(None)
-                            my_serial.port = tty_device.device_node
-                            my_serial.open()
-                            my_serial.write("enter bootloader\n".encode())
-                            my_serial.close()
-                            time.sleep(4) #wait for the device to enter bootloader mode
-                            if arguments.verbose:
-                                print("Info: Device was sent into bootloader mode.")
+            # # TODO: Maybe we could send the device into bootloader mode directly from inside the container?
+            # import pyudev # Import only here, as this file is also used on github runners without hardware access. So this is not installed and won't be used there.
+            # import serial
+            # udev = pyudev.Context()
+            # for usb_device in  udev.list_devices(subsystem="usb"):
+            #     if usb_device.attributes.get('manufacturer') == b'github.com/erichstuder' and usb_device.attributes.get('product') == b'433MHz_to_MQTT':
+            #         for tty_device in  udev.list_devices(subsystem="tty"):
+            #             if tty_device.sys_path.startswith(usb_device.sys_path):
+            #                 my_serial = serial.Serial(None)
+            #                 my_serial.port = tty_device.device_node
+            #                 my_serial.open()
+            #                 my_serial.write("enter bootloader\n".encode())
+            #                 my_serial.close()
+            #                 time.sleep(4) #wait for the device to enter bootloader mode
+            #                 if arguments.verbose:
+            #                     print("Info: Device was sent into bootloader mode.")
 
-            commands = 'cd firmware && cargo run'
+            commands += ' && cargo run'
         elif arguments.set_version_from_tag:
+            print('this command must be corrected first!')
+            exit(1)
             commands = 'cd firmware && cargo set-version $(git tag | sed "s/^.//")'
 
     this_file_dir = pathlib.Path(__file__).resolve().parent
